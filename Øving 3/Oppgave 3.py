6aebe7eb31e7cef504c9733b054f207178f7aa90
trondhumbor <trondhumbor@gmail.com>
2014-09-16 09:30:42 +0200

    Slight modification to exercise 3 to conform with the assignment.

--- Øving 3/Oppgave 3.py
+++ Øving 3/Oppgave 3.py
@@ -1,17 +1,19 @@
 import math
 
 def closestToN(n):
-    iterativeSum = 0
-    currentInteger = 1
-    elements = 0
+	iterativeSum = 0
+	currentInteger = 1
+	elements = 0
 
-    # math.pow tilsvarer **
-    while (iterativeSum + math.pow(currentInteger, 2)) < n:
-        iterativeSum += math.pow(currentInteger, 2)
-        currentInteger += 1
-        elements += 1
+	# math.pow tilsvarer **
+	while (iterativeSum + math.pow(currentInteger, 2)) < n:
+		iterativeSum += math.pow(currentInteger, 2)
+		currentInteger += 1
+		elements += 1
 
-    return iterativeSum, elements
+	return iterativeSum, elements
 
 if __name__ == "__main__":
-    print(closestToN(10))
+	n = int(input("Skriv inn n: "))
+	returnedData = closestToN(n)
+	print("Summen er", returnedData[0], "med", returnedData[1], "elementer")
